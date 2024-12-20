dcb5d5ba131c96f67eb06afca5436f8345b62d6a
Tiago <Tssdj13@gmail.com>
2024-12-20 15:35:49 -0300

    struct para Registro

--- model/model.go
+++ model/model.go
@@ -7,3 +7,9 @@ type LoginRequest struct {
 	Email    string `json:"email"`
 	Password string `json:"password"`
 }
+
+type RegisterRequest struct {
+	Name     string `json:"name"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
