b24450eb8e06f6ecf2f6454c61e9b5d40669bc62
Tiago <Tssdj13@gmail.com>
2024-12-27 14:52:42 -0300

    add Estrutura do modelo de Usuario

--- model/model.go
+++ model/model.go
@@ -1,14 +1,26 @@
 package model
 
+// estrutura para Mensagens
 type Message struct {
 	Message string
 }
+
+// estrutura para o Login
 type LoginRequest struct {
 	Email    string `json:"email"`
 	Password string `json:"password"`
 }
 
+// estrutura para o registro
 type RegisterRequest struct {
+	Name     string `json:"name"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+// estrutura para o Banco de Dados
+type User struct {
+	Id       uint   `json:"id"`
 	Name     string `json:"name"`
 	Email    string `json:"email"`
 	Password string `json:"password"`
