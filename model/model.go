7a172e853253eca1ed425091e4aa1a91019899a7
Tiago <Tssdj13@gmail.com>
2024-12-27 18:11:53 -0300

    add tag do bd para funcionar com o *sqlx

--- model/model.go
+++ model/model.go
@@ -17,17 +17,17 @@ type LoginRequest struct {
 
 // estrutura para o registro
 type RegisterRequest struct {
-	Name       string    `json:"name"`
+	Name       string    `json:"nome"`
 	Email      string    `json:"email"`
 	Password   string    `json:"password"`
 	Created_at time.Time `json:"created_at"`
 }
 
-// estrutura para o Banco de Dados
+// Estrutura para o Banco de Dados
 type User struct {
-	Id         uint      `json:"id"`
-	Name       string    `json:"name"`
-	Email      string    `json:"email"`
-	Password   string    `json:"password"`
-	Created_at time.Time `json:"created_at"`
+	Id         uint      `json:"id" db:"id"`                 // db:"id" mapeia para a coluna 'id' no banco
+	Name       string    `json:"nome" db:"nome"`             // db:"nome" mapeia para a coluna 'nome' no banco
+	Email      string    `json:"email" db:"email"`           // db:"email" mapeia para a coluna 'email' no banco
+	Password   string    `json:"password" db:"password"`     // db:"password" mapeia para a coluna 'password' no banco
+	Created_at time.Time `json:"created_at" db:"created_at"` // db:"created_at" mapeia para a coluna 'created_at' no banco
 }
