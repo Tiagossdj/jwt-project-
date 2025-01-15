fd7b1773ab3025bb353e0045069b9cd5aef72400
Tiago <Tssdj13@gmail.com>
2025-01-15 14:26:09 -0300

    add Testes da func Register

--- handlers/auth_test.go
+++ handlers/auth_test.go
@@ -47,7 +47,8 @@ func TestLogin_Success(t *testing.T) {
 	assert.NoError(t, mock.ExpectationsWereMet())
 }
 
-// teste com credenciais Inválidas
+// teste com credenciais Inválidas!
+
 func TestLogin_InvalidCredencials(t *testing.T) {
 	e := echo.New()
 	req := httptest.NewRequest(http.MethodPost, "/auth/login", strings.NewReader(`{"email":"invalid@user.com", "password":"wrongpass"}`))
@@ -93,4 +94,75 @@ func TestLogin_MissingData(t *testing.T) {
 		assert.Equal(t, http.StatusUnauthorized, rec.Code)
 		assert.Contains(t, rec.Body.String(), "invalid credentials!")
 	}
+}
+
+// Teste de Register com Sucesso!
+
+func TestRegister_Success(t *testing.T) {
+	e := echo.New()
+
+	// simula requisição http com email e a senha válida!
+	req := httptest.NewRequest(http.MethodPost, "/auth/register", strings.NewReader(`{"nome":"test user", "email":"test@user.com", "password":"123456"}`))
+	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+	rec := httptest.NewRecorder()
+	c := e.NewContext(req, rec)
+
+	// simula mock do Banco de Dados
+	mockDB, mock, err := sqlmock.New()
+	if err != nil {
+		t.Fatalf("Failed to create mock db:%v", err)
+	}
+	db := sqlx.NewDb(mockDB, "postgres")
+
+	//expectativa para o SELECT do email
+	mock.ExpectQuery("SELECT \\* FROM usuario WHERE email = \\$1").
+		WithArgs("test@user.com").
+		WillReturnError(sql.ErrNoRows) //simula que o email não está registrado no banco.
+
+	//Caso o modelo de usuario seja registrado corretamente!
+	mock.ExpectExec("INSERT INTO usuario"). // o comando de inserção de dados
+						WithArgs("test user", "test@user.com", sqlmock.AnyArg()). // user, email e hash password(AnyArg() pois o hash muda a cada chamada da função!)
+						WillReturnResult(sqlmock.NewResult(1, 1))                 // retorna sucesso na inserção.
+
+	// executa o handler de register
+	if assert.NoError(t, Register(c, db)) { //Chama a função Register passando o contexto e a DB simulada
+		assert.Equal(t, http.StatusCreated, rec.Code)
+		assert.Contains(t, rec.Body.String(), "successfully registered user")
+	}
+
+	assert.NoError(t, mock.ExpectationsWereMet())
+
+}
+
+// Teste de Register com Email Existente!
+
+func TestRegister_EmailExists(t *testing.T) {
+	e := echo.New()
+
+	// simula requisição http com email e a senha válida!
+	req := httptest.NewRequest(http.MethodPost, "/auth/register", strings.NewReader(`{"nome":"test user", "email":"test@user.com", "password":"123456"}`))
+	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+	rec := httptest.NewRecorder()
+	c := e.NewContext(req, rec)
+
+	// simula mock do Banco de Dados
+	mockDB, mock, err := sqlmock.New()
+	if err != nil {
+		t.Fatalf("Failed to create mock db:%v", err)
+	}
+	db := sqlx.NewDb(mockDB, "postgres")
+
+	//expectativa para o SELECT do email
+	mock.ExpectQuery("SELECT \\* FROM usuario WHERE email = \\$1").
+		WithArgs("test@user.com").
+		WillReturnRows(sqlmock.NewRows([]string{"email"}).
+			AddRow("test@user.com"))
+
+	// Executar handler de Register
+	if assert.NoError(t, Register(c, db)) { //Chama a função Register passando o contexto e a DB simulada
+		assert.Equal(t, http.StatusConflict, rec.Code)
+		assert.Contains(t, rec.Body.String(), "Email already registered")
+	}
+
+	assert.NoError(t, mock.ExpectationsWereMet()) // Verifica se todas as expectativas de mock foram atendidas
 }
