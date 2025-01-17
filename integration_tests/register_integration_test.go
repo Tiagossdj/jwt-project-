f5c89b1841ce2312041281cb15b7becafa8722e2
Tiago <Tssdj13@gmail.com>
2025-01-17 16:42:00 -0300

    Mudança dos testes para 1 teste com subtestes

--- integration_tests/register_integration_test.go
+++ integration_tests/register_integration_test.go
@@ -16,45 +16,57 @@ func TestRegister_Integration(t *testing.T) {
 	db := SetupTestDataBase(t)
 	defer CleanUpTestDataBase(db)
 
-	// Configura o echo e cria o contexto para o teste
-	e := echo.New()
-	req := httptest.NewRequest(http.MethodPost, "/auth/register", strings.NewReader(`{"nome":"Test User", "email":"test@example.com", "password":"123456"}`))
-	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
-	rec := httptest.NewRecorder()
-	c := e.NewContext(req, rec)
+	// Teste 1: Teste de Registro com dados válidos!
 
-	// Executa o handler Register
-	if assert.NoError(t, handlers.Register(c, db)) {
-		assert.Equal(t, http.StatusCreated, rec.Code)
-		assert.Contains(t, rec.Body.String(), "successfully registered user")
-	}
+	t.Run("TestRegister_ValidData", func(t *testing.T) {
+		// Limpa Banco de dados antes de cada subteste
+		db.Exec("Truncate table usuario restart identity cascade")
 
-	// verifica no banco se o usuário foi Registrado
-	var count int
-	err := db.QueryRow("select count(*) from usuario where email= $1", "test@example.com").Scan(&count)
-	if err != nil {
-		t.Fatalf("Error to Verify test Database:%v", err)
-	}
-	assert.Equal(t, 1, count)
-}
+		// Configura o echo e cria o contexto para o teste
+		e := echo.New()
+		req := httptest.NewRequest(http.MethodPost, "/auth/register", strings.NewReader(`{"nome":"Test User", "email":"test@example.com", "password":"123456"}`))
+		req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+		rec := httptest.NewRecorder()
+		c := e.NewContext(req, rec)
 
-func TestRegister_EmailAlreadyExists(t *testing.T) { // Configura o banco de dados de testes
-	db := SetupTestDataBase(t)
-	defer CleanUpTestDataBase(db)
+		// Executa o handler Register
+		if assert.NoError(t, handlers.Register(c, db)) {
+			assert.Equal(t, http.StatusCreated, rec.Code)
+			assert.Contains(t, rec.Body.String(), "successfully registered user")
+		}
 
-	// Cria dados necessários para teste
-	PopulateTestData(db)
+		// verifica no banco se o usuário foi Registrado
+		var count int
+		err := db.QueryRow("select count(*) from usuario where email= $1", "test@example.com").Scan(&count)
+		if err != nil {
+			t.Fatalf("Error to Verify test Database:%v", err)
+		}
+		assert.Equal(t, 1, count)
 
-	// Configura o echo e cria o contexto para o teste
-	e := echo.New()
-	req := httptest.NewRequest(http.MethodPost, "/auth/register", strings.NewReader(`{"nome":"Test User", "email":"test@example.com", "password":"123456"}`))
-	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
-	rec := httptest.NewRecorder()
-	c := e.NewContext(req, rec)
+	})
 
-	// Executa o handler Register
-	if assert.NoError(t, handlers.Register(c, db)) {
-		assert.Equal(t, http.StatusConflict, rec.Code)
-		assert.Contains(t, rec.Body.String(), "Email already registered")
-	}
+	// Teste 2: Se ja existir esse email no Banco de dados!
+	t.Run("TestRegister_EmailAlreadyExists", func(t *testing.T) {
+
+		// Limpa Banco de dados antes de cada subteste
+		db.Exec("Truncate table usuario restart identity cascade")
+
+		// Cria dados necessários para teste
+		PopulateTestData(db)
+
+		// Configura o echo e cria o contexto para o teste
+		e := echo.New()
+		req := httptest.NewRequest(http.MethodPost, "/auth/register", strings.NewReader(`{"nome":"Test User", "email":"test@example.com", "password":"123456"}`))
+		req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+		rec := httptest.NewRecorder()
+		c := e.NewContext(req, rec)
+
+		// Executa o handler Register
+		if assert.NoError(t, handlers.Register(c, db)) {
+			assert.Equal(t, http.StatusConflict, rec.Code)
+			assert.Contains(t, rec.Body.String(), "Email already registered")
+		}
+
+	})
+
 }
